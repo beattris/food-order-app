ca8172321c30d5b838e79e1db01e15172c269146
Beattris <beatriceebirim@gmail.com>
2022-01-24 22:14:49 +0100

    finished

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,36 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styles from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
+  
+  const { items } = cartCtx;
 
-  const numberOfCartItems = cartCtx.items.reduce(
+  const numberOfCartItems = items.reduce(
     (currentNumber, item) => {
       return currentNumber + item.amount;
     }, 0);
+  
+  const btnStyles = `${styles.button} ${btnIsHighlighted ? styles.bump : ''}` ;
+
+  useEffect(() => {
+    if (items.length === 0){
+      return;
+    }
+    const timer = setBtnIsHighlighted(true);
+    setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [items])  
 
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={btnStyles} onClick={props.onClick}>
       <span className={styles.icon}>
           <CartIcon />
       </span>
