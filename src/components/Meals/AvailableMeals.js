ba6edb4f85c00314d514c3b2c546bc94febc8dab
Beattris <beatriceebirim@gmail.com>
2022-02-24 12:51:58 +0100

    connected to firebase to fetch the meals data

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,43 +1,41 @@
 import styles from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
-
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+import { useEffect, useState } from "react";
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
-      <MealItem 
-        id={meal.id}
-        key={meal.id} 
-        description={meal.description} 
-        name={meal.name} 
-        price={meal.price}
-      />
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    // TO USE ASYNC/AWAIT IN useEffect, CREATE A FUNCTION THAT HOLDS THE PROMISE, THEN CALL THE FUNCTION INSIDE OF useEffect.
+    const fetchMeals = async () => {
+      const response = await fetch('https://food-order-app-10c85-default-rtdb.firebaseio.com/meals.json');
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData){
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price
+        });
+      }
+      setMeals(loadedMeals);
+    }
+    // CALLING THE FUNCTION
+    fetchMeals();
+  }, [])
+
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      id={meal.id}
+      key={meal.id}
+      description={meal.description}
+      name={meal.name}
+      price={meal.price}
+    />
   ));
 
   return (
